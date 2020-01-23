06802b0a0daa883118a892c931820b12ddbeb254
Michal Bureš <michal.bures@travelportgds.cz>
2020-01-23 07:41:09 +0100

    Fix offline db access

--- src/database/getDocumentClient.ts
+++ src/database/getDocumentClient.ts
@@ -2,11 +2,12 @@ import { DynamoDB } from 'aws-sdk'
 const isOffline: string | undefined = process.env.IS_OFFLINE
 
 export const getDocumentClient = (): DynamoDB.DocumentClient => {
-  if (isOffline === 'true') {
+  if (isOffline) {
     return new DynamoDB.DocumentClient({
-      // tslint:disable-next-line:no-http-string
       endpoint: 'http://localhost:3001',
-      region: 'localhost'
+      region: 'localhost',
+      accessKeyId: 'DEFAULT_ACCESS_KEY',
+      secretAccessKey: 'DEFAULT_SECRET',
     })
   }
 
