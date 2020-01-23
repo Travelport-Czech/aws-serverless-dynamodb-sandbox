8fce3bd6ce0179353b1b0c152faf46bd09cb7c32
Michal Bureš <michal.bures@travelportgds.cz>
2020-01-23 07:56:11 +0100

    Use ts-loader instead babel

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,8 @@
+const os = require('os')
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 const slsw = require('serverless-webpack')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 const config = {
   mode: 'production',
@@ -14,34 +16,36 @@ const config = {
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    plugins: [new TsconfigPathsPlugin()],
   },
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '../.webpack'),
     filename: '[name].js',
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
   },
   target: 'node',
   module: {
     rules: [
       {
-        test: /\.(ts|js)x?$/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['@babel/preset-env', { targets: { node: '10' } }],
-            '@babel/typescript'
-          ],
-          plugins: [
-            '@babel/proposal-class-properties',
-            '@babel/proposal-object-rest-spread',
-            ["module-resolver", {
-              "root": ["./src"],
-              "alias": {
-                "@": "./src"
-              }
-            }],
-          ]
-        }
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'cache-loader',
+          },
+          {
+            loader: 'thread-loader',
+            options: {
+              workers: os.cpus().length - 1,
+            },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true,
+            },
+          },
+        ],
       }
     ],
   },
